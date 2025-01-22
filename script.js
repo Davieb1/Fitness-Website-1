4964ea9a353f6899c693226971d9333d9d05aa19
Davieb1 <121017739+Davieb1@users.noreply.github.com>
2025-01-22 17:35:59 +0000

    Add files via upload
    Initial Commit - Fitness Website #1

---
+++ script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// Typing Text 
+const typed = new Typed('.multiple-text', {
+    strings: ['Physical Fitness', 'Weight Gain', 'Strength Training', 'Fat Loss', 'Weight Lifting', 'Running'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+});
